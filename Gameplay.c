198cb4063960ac384fa9f899b017e21f48144b0b
pyod1212 <79109164+pyod1212@users.noreply.github.com>
2021-03-01 15:30:59 -0500

    Add files via upload

--- Gameplay.c
+++ Gameplay.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //open powershell to run
 //then in Visual Studio Code click 'Terminal'->'Run Task' ... gcc.exe ... then in Powershell type  ./Gameplay while in the correct directory
@@ -10,8 +11,8 @@
 struct Game{
     int board[8][8];
     int x0, y0, x1, y1, xj, yj;
-    int turn;
-    int jump, must_jump;
+    int turn, jump, must_jump;
+
 };
 
 //functions
@@ -44,7 +45,7 @@ void main(void){
 
                     //this is for testing only
                     if(pt->x0 == 8){
-                        int c[8][8] = {{0,0,1,0,0,1,0,0},{0,0,0,0,0,0,2,0},{0,0,1,0,0,0,0,0},{0,2,0,0,0,0,2,0},{0,0,0,0,1,0,0,0},{0,0,0,0,0,2,0,0},{0,0,0,0,0,0,0,0},{2,0,0,0,0,0,0,0}};
+                        int c[8][8] = {{0,0,1,0,1,0,0,0},{0,1,0,0,0,2,0,0},{0,0,0,0,0,0,0,0},{0,2,0,1,0,2,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,2,0,0},{0,0,1,0,2,0,0,0},{0,2,0,0,0,0,0,0}};
                         printf("\nsetting up jumping scenario...\n");
                         for( int i=0; i<8; i++){
                             for (int j=0; j<8; j++){
@@ -70,9 +71,11 @@ void main(void){
             }
 
             while(1){
-                //for testing purposes
                 
+                //for testing purposes
                 if(pt->x0 == 8){
+                    pt->x1 = 0;
+                    pt->y1 = 0;
                     break;
                 }
 
@@ -135,7 +138,7 @@ void SwitchTurn(struct Game *p){
 }
 
 int CheckInputOne(struct Game *p){
-    if(p->board[p->y0][p->x0] != p->turn)
+    if(p->board[p->y0][p->x0] != p->turn && ((p->turn == 1 && p->board[p->y0][p->x0] !=3) || (p->turn == 2 && p->board[p->y0][p->x0] != 4) ))
         return 0;
     else
         return 1;
@@ -152,7 +155,7 @@ int CheckInputTwo(struct Game *p){
     }
     if(p->x0 == p->x1 && p->y0 == p->y1) //same as first coords...choose different first coords
         return 2;
-    else if(p->board[p->y1][p->x1] != 0 || abs(p->x0 - p->x1) != abs(p->y0 - p->y1) || (p->turn == 1 && p->y1-p->y0 < 0 ) || (p->turn == 2 && p->y1-p->y0 > 0) || ( (abs(p->x0-p->x1) != 1) && (abs(p->y0-p->y1) != 1) && p->jump == 0)) //rule checking should be good for normal pieces
+    else if(p->board[p->y1][p->x1] != 0 || abs(p->x0 - p->x1) != abs(p->y0 - p->y1) || (p->turn == 1 && p->board[p->y0][p->x0] != 3 && p->y1-p->y0 < 0 ) || (p->turn == 2 && p->board[p->y0][p->x0] != 4 && p->y1-p->y0 > 0) || ( (abs(p->x0-p->x1) != 1) && (abs(p->y0-p->y1) != 1) && p->jump == 0)) //rule checking should be good for normal pieces
         return 0;
     else
         return 1;
@@ -195,7 +198,41 @@ void CheckMultipleJumps(struct Game *p){
         printf("\nchecking multiple jumps\n");
         if(p->turn == 1){
             printf("\nchecking jumps for p1...\n");
-            if(p->y1 < 6 && p-> x1 > 1 && p->board[p->y1+1][p->x1-1] == 2){
+
+            //player 1 king and jumping backwards
+            if(p->board[p->y1][p->x1] == 3){
+                if(p->y1 > 1 && p->x1 > 1 && (p->board[p->y1-1][p->x1-1] == 2 || p->board[p->y1-1][p->x1-1] == 4)){
+                    if(p->board[p->y1-2][p->x1-2] == 0){ //jump available over this piece
+                        printf("\nAnother jump available\n");
+                        SwitchTurn(p);
+                        p->must_jump =1;
+                    }else{ //no consecutive jump over this piece
+                        printf("\nNo more jumps available\n");
+                        p->must_jump = 0;
+                        p->jump = 0;
+                    }
+                
+                }else if(p->y1 > 1 && p->x1 < 6 && (p->board[p->y1-1][p->x1+1] == 2 || p->board[p->y1-1][p->x1+1] == 4)){
+                    if(p->board[p->y1-2][p->x1+2] == 0){ //jump available over this piece
+                        printf("\nAnother jump available\n");
+                        SwitchTurn(p);
+                        p->must_jump =1;
+                    }else{ //no consecutive jump over this piece
+                        printf("\nNo more jumps available\n");
+                        p->must_jump = 0;
+                        p->jump = 0;
+                    }
+                    
+                }else{ //no nearby opposite peices
+                    printf("\nNo more jumps available\n");
+                    p->must_jump = 0;
+                    p->jump = 0;
+                }
+
+            
+                
+            //player 1 jumping forward to the left
+            }else if(p->y1 < 6 && p-> x1 > 1 && (p->board[p->y1+1][p->x1-1] == 2 || p->board[p->y1+1][p->x1-1] == 4)){
                 if(p->board[p->y1+2][p->x1-2] == 0){ //possible jump available
                     printf("\nAnother jump available1\n");
                     SwitchTurn(p);
@@ -205,8 +242,8 @@ void CheckMultipleJumps(struct Game *p){
                     p->must_jump = 0;
                     p->jump = 0;
                 }
-                
-            }else if(p->y1 < 6 && p->x1 < 6 && p->board[p->y1+1][p->x1+1] == 2){
+            //player 1 jumping forward to the right   
+            }else if(p->y1 < 6 && p->x1 < 6 && (p->board[p->y1+1][p->x1+1] == 2 || p->board[p->y1+1][p->x1+1] == 4)){
                 if(p->board[p->y1+2][p->x1+2] == 0){ //possible jump available
                     printf("\nAnother jump available2\n");
                     SwitchTurn(p);
@@ -221,9 +258,40 @@ void CheckMultipleJumps(struct Game *p){
                 p->must_jump = 0;
                 p->jump = 0;
             }
+
+        //checking player 2 jumps
         }else if(p->turn == 2){
             printf("\nchecking jumps for p2...\n");
-            if(p->y1 > 1 && p->x1 > 1 && p->board[p->y1-1][p->x1-1] == 1){
+
+            //player 2 king jumping backwards
+            if(p->board[p->y1][p->x1] == 4){
+                if(p->y1 < 6 && p-> x1 > 1 && (p->board[p->y1+1][p->x1-1] == 1 || p->board[p->y1+1][p->x1-1] == 3)){
+                    if(p->board[p->y1+2][p->x1-2] == 0){ //possible jump available
+                        printf("\nAnother jump available1\n");
+                        SwitchTurn(p);
+                        p->must_jump =1;
+                    }else{ //no consecutive jump available+
+                        printf("\nNo more jumps available\n");
+                        p->must_jump = 0;
+                        p->jump = 0;
+                    }
+                } else{
+                    printf("\nNo more jumps available\n");
+                    p->must_jump = 0;
+                    p->jump = 0;
+                }   
+            }else if(p->y1 < 6 && p->x1 < 6 && (p->board[p->y1+1][p->x1+1] == 1 || p->board[p->y1+1][p->x1+1] == 3)){
+                if(p->board[p->y1+2][p->x1+2] == 0){ //possible jump available
+                    printf("\nAnother jump available2\n");
+                    SwitchTurn(p);
+                    p->must_jump =1;
+                }else{ //no consecutive jump available
+                    printf("\nNo more jumps available\n");
+                    p->must_jump = 0;
+                    p->jump = 0;
+                }            
+
+            }else if(p->y1 > 1 && p->x1 > 1 && (p->board[p->y1-1][p->x1-1] == 1 || p->board[p->y1-1][p->x1-1] == 3)){
                 if(p->board[p->y1-2][p->x1-2] == 0){ //jump available over this piece
                     printf("\nAnother jump available\n");
                     SwitchTurn(p);
@@ -234,7 +302,7 @@ void CheckMultipleJumps(struct Game *p){
                     p->jump = 0;
                 }
                 
-            }else if(p->y1 > 1 && p->x1 < 6 && p->board[p->y1-1][p->x1+1] == 1){
+            }else if(p->y1 > 1 && p->x1 < 6 && (p->board[p->y1-1][p->x1+1] == 1 || p->board[p->y1-1][p->x1+1] == 3)){
                 if(p->board[p->y1-2][p->x1+2] == 0){ //jump available over this piece
                     printf("\nAnother jump available\n");
                     SwitchTurn(p);
@@ -255,17 +323,32 @@ void CheckMultipleJumps(struct Game *p){
 
 }
 
+
 void UpdateBoard(struct Game *p){
     
     if(p->x0 == 8)  //for testing
         return;
 
+    p->board[p->y1][p->x1] = p->board[p->y0][p->x0];
     p->board[p->y0][p->x0] = 0;
-    p->board[p->y1][p->x1] = p->turn;
-    printf("start: (%d , %d)\t end: (%d , %d)",p->x0,p->y0,p->x1,p->y1);
+    printf("\nstart: (%d , %d)\t end: (%d , %d)",p->x0,p->y0,p->x1,p->y1);
     if(p->jump == 1){
         p->board[p->yj][p->xj] = 0;
         printf("\nJumped piece at %d %d",p->xj,p->yj);
+    }
+    //check for kinging
+    for( int i=0; i<8; i++){
+        if(p->board[7][i] == 1){
+            p->board[7][i] = 3;
+            printf("\nPlayer 1 has been kinged\n");
+        }
+            
+        if(p->board[0][i] == 2){
+            p->board[0][i] = 4;
+            printf("\nPlayer 2 has been kinged\n");
+        }
+    
+
     }
 }
 
