5e2607f8a12455e9d95db2e3a095b0c267973c68
Thomas Driscoll <tjd61@pitt.edu>
2021-03-01 11:18:23 -0500

    Set up toggle with one push button

--- NeoPixel_Test/NeoPixel_Test.ino
+++ NeoPixel_Test/NeoPixel_Test.ino
@@ -1,12 +1,16 @@
 #include "NDPixel.h"
 
-NDPixel pixels(8, 8);
+NDPixel pixels(8, 16);
+
+#define Button 2
+boolean toggleState = false;
 
 void setup() {
   OCR0A = 0xAF; //Set Timer 0 Counter Register Value
   TIMSK0 |= _BV(OCIE0A);
 
-  pixels.setBrightness(50);
+  pinMode(Button, INPUT_PULLUP);
+  pixels.setBrightness(255);
 }
 
 //INTERRUPT HANDLING
@@ -15,7 +19,19 @@ SIGNAL(TIMER0_COMPA_vect) { //Timer 0 Counter Comparison
 }
 
 void loop() {
-  pixels.demo();
   //pixels.demoChase(0, 255, 0, 4);
-  delay(1000);
+  if(digitalRead(Button) == LOW) {
+    toggleState = !toggleState;
+    if(!toggleState) {
+      for(int i = 0; i < 16; i++) {
+        pixels.setPixel(i, 255, 255, 255);
+      }
+    }
+    else {
+      for(int i = 0; i < 16; i++) {
+        pixels.setPixel(i, 0, 0, 0);
+      }
+    }
+    delay(300);
+  }
 }
